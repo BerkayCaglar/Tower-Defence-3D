eebb92fd8923f6328a87f4a44255e6f953a7a348
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2022-05-29 22:44:33 +0300

    Updated

---
+++ Tower Defence 3D/Assets/Game/Scripts/MTAnimationController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTAnimationController : MonoBehaviour
+{
+    private Animator leftBarrelAnimator;
+    private Animator rightBarrelAnimator;
+    private MTSpawnBullet mTSpawnBullet;
+    
+    private void Start() {
+        FindBarrelAnimators();
+        FindMTSpawnBullet();
+    }
+
+    public void PlayRightBarrelAnimation()
+    {
+        rightBarrelAnimator.SetBool("IsAttacking",true);
+    }
+    public void PlayLeftBarrelAnimation()
+    {
+        leftBarrelAnimator.SetBool("IsAttacking",true);
+    }
+    public void StopBarrelAnimation()
+    {
+        rightBarrelAnimator.SetBool("IsAttacking",false);
+        leftBarrelAnimator.SetBool("IsAttacking",false);
+    }
+    private void FindMTSpawnBullet()
+    {
+        mTSpawnBullet = gameObject.GetComponent<MTSpawnBullet>();
+    }
+    private void FindBarrelAnimators()
+    {
+        leftBarrelAnimator = GameObject.Find("MT-Left Barrel").GetComponent<Animator>();
+        rightBarrelAnimator = GameObject.Find("MT-Right Barrel").GetComponent<Animator>();
+    }
+}
--- Tower Defence 3D/Assets/Game/Scripts/MTSpawnBullet.cs
+++ Tower Defence 3D/Assets/Game/Scripts/MTSpawnBullet.cs
@@ -6,13 +6,49 @@ public class MTSpawnBullet : MonoBehaviour
 {
     [SerializeField]
     private GameObject bullet;
+    private GameObject leftBarrel;
+    private GameObject rightBarrel;
+    private MTAnimationController mTAnimationController;
+    private bool shotOpen=true;
     void Start()
     {
-        InvokeRepeating("SpawnBullet",0f,1f);
+        FindBarrelObjects();
+        InvokeRepeating("SpawnManager",0f,0.5f);
+        FindMTAnimationController();
     }
 
-    private void SpawnBullet()
+    private void SpawnLeftBullet()
     {
-        Instantiate(bullet,new Vector3(0.25f,1.2f,1.3f),gameObject.transform.rotation);
+        Instantiate(bullet,leftBarrel.transform.position,gameObject.transform.rotation);
+        shotOpen = false;
     }
+    private void SpawnRightBullet()
+    {
+        Instantiate(bullet,rightBarrel.transform.position,gameObject.transform.rotation);
+        shotOpen = true;
+    }
+    
+    private void SpawnManager()
+    {
+        if (shotOpen)
+        {
+            SpawnLeftBullet();
+            mTAnimationController.PlayLeftBarrelAnimation();
+        }
+        else
+        {
+            SpawnRightBullet();
+            mTAnimationController.PlayRightBarrelAnimation();
+        }
+    }
+    private void FindBarrelObjects()
+    {
+        leftBarrel = GameObject.Find("MT-Left Barrel");
+        rightBarrel = GameObject.Find("MT-Right Barrel");
+    }
+    private void FindMTAnimationController()
+    {
+        mTAnimationController = gameObject.GetComponent<MTAnimationController>();
+    }
+
 }   
